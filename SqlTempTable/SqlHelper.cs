16e23fb3ce0dfdb1426489e690e87cb472e51852
Deniz OZOGUL <deniz.ozogul@bilgeadam.com>
2020-04-25 13:08:44 +0300

    first commit

---
+++ SqlTempTable/SqlHelper.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SqlTempTable
+{
+    public static class SqlHelper
+    {
+        private static Dictionary<Type, SqlDbType> typeMap;
+        static SqlHelper()
+        {
+            typeMap = new Dictionary<Type, SqlDbType>();
+
+            typeMap[typeof(string)] = SqlDbType.NVarChar;
+            typeMap[typeof(char[])] = SqlDbType.NVarChar;
+            typeMap[typeof(byte)] = SqlDbType.TinyInt;
+            typeMap[typeof(short)] = SqlDbType.SmallInt;
+            typeMap[typeof(int)] = SqlDbType.Int;
+            typeMap[typeof(long)] = SqlDbType.BigInt;
+            typeMap[typeof(byte[])] = SqlDbType.Image;
+            typeMap[typeof(bool)] = SqlDbType.Bit;
+            typeMap[typeof(DateTime)] = SqlDbType.DateTime2;
+            typeMap[typeof(DateTimeOffset)] = SqlDbType.DateTimeOffset;
+            typeMap[typeof(decimal)] = SqlDbType.Money;
+            typeMap[typeof(float)] = SqlDbType.Real;
+            typeMap[typeof(double)] = SqlDbType.Float;
+            typeMap[typeof(TimeSpan)] = SqlDbType.Time;
+            /* ... and so on ... */
+        }
+
+        // Non-generic argument-based method
+        public static SqlDbType GetDbType(Type giveType)
+        {
+            // Allow nullable types to be handled
+            giveType = Nullable.GetUnderlyingType(giveType) ?? giveType;
+
+            if (typeMap.ContainsKey(giveType))
+            {
+                return typeMap[giveType];
+            }
+            if (giveType.BaseType == typeof(Enum))
+            {
+                return SqlDbType.Int;
+            }
+
+            throw new ArgumentException($"{giveType.FullName} is not a supported .NET class");
+        }
+
+        // Generic version
+        public static SqlDbType GetDbType<T>()
+        {
+            return GetDbType(typeof(T));
+        }
+    }
+}
---
+++ SqlTempTable/TempTable.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SqlTempTable
+{
+    public static class TempTable<T> where T : new()
+    {
+        public static string CreateQuery(string TempTableName)
+        {
+            T _tempClass = new T();
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append("CREATE TABLE #").Append(TempTableName).Append("(").Append("\n");
+            var length = _tempClass.GetType().GetProperties().Length - 1;
+            for (int i = 0; i < length; i++)
+            {
+                var pi = _tempClass.GetType().GetProperties()[i];
+                stringBuilder.Append("\t").Append(pi.Name).Append(" ").Append(SqlHelper.GetDbType(pi.PropertyType)).Append(",").Append("\n");
+            }
+            var lastpi = _tempClass.GetType().GetProperties()[length];
+            stringBuilder.Append("\t").Append(lastpi.Name).Append(" ").Append(SqlHelper.GetDbType(lastpi.PropertyType)).Append("\n").Append(")");
+            return stringBuilder.ToString();
+        }
+        public static string DropQuery(string TempTableName)
+        {
+
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append("DROP TABLE #").Append(TempTableName);
+            return stringBuilder.ToString();
+        }
+    }
+}
